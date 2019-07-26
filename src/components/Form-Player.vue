19f1bffcbc9333bc51c7ce8491a466c37436d789
irshadi <bagasputro.irshadi@gmail.com>
2019-07-26 11:41:02 +0700

    masih ada bug

---
+++ src/components/Form-Player.vue
@@ -0,0 +1,45 @@
+<template>
+  <form action="">
+    <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Welcome !</p>
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Enter Username">
+          <b-input
+            type="text"
+            v-model="username"
+            placeholder="Your Username"
+            required>
+          </b-input>
+        </b-field>
+
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" type="button" @click="$parent.close()">Close</button>
+        <button @click.prevent="addPlayer" class="button is-primary">Join Room</button>
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props : ['selected'],
+  data() {
+    return {
+      username: null,
+    }
+  },
+  methods: {
+    addPlayer() {
+      const payload = {
+        roomid : this.selected,
+        name : this.username
+      }
+      this.$store.dispatch('insertPlayer', payload)
+    }
+  }
+}
+
+</script>
